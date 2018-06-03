dae299cdd357145a8c951660750ea0994a778143
Thalandor <thalandor1985@hotmail.com>
2018-06-03 23:32:40 +0200

    Update config.service.ts

--- src/app/shared/utils/config.service.ts
+++ src/app/shared/utils/config.service.ts
@@ -6,7 +6,7 @@ export class ConfigService {
     _apiURI : string;
  
     constructor() {
-        this._apiURI = 'http://ivancoinwebapi.azurewebsites.net/';
+        this._apiURI = 'http://ivancoinwebapi.azurewebsites.net/api';
      }
  
      getApiURI() {
